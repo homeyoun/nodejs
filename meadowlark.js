f2f265616e0c64bace2283351cfef8f2e4e0ece6
homeyoun <homeyoun@naver.com>
2023-11-18 12:02:32 +0900

    라우팅 작업

--- meadowlark.js
+++ meadowlark.js
@@ -1,28 +1,44 @@
 const express = require("express");
+const expressHandlebars = require("express-handlebars");
 const app = express();
 const port = process.env.PORT || 3000;
 
+const fortunes = [
+  "Conquer your fears or they will conquer you.",
+  "Rivers need springs.",
+  "Do not fear what you don't know.",
+  "You will have a pleasant surprise.",
+  "Whenever possible,keep it simple.",
+];
+
+app.engine(
+  "handlebars",
+  expressHandlebars({
+    defaultLayout: "main",
+  })
+);
+
+app.set("view engine", "handlebars");
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
-  res.type("text/plain");
-  res.send("Meadowlark Travel");
+  res.render("home");
 });
 
 app.get("/about", (req, res) => {
-  res.type("text/plain");
-  res.send("About Meadowlark Travel");
+  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render("about", { fortune: randomFortune });
 });
 
 app.use((req, res) => {
-  res.type("text/plain");
   res.status(404);
-  res.send("404 - Not Found");
+  res.render("404");
 });
 
 app.use((err, req, res, next) => {
   console.error(err.message);
-  res.type("text/plain");
   res.status(500);
-  res.send("500 - Server Error");
+  res.render("500");
 });
 
 app.listen(port, () =>
